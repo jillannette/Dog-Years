a1c8f6237ecd1df028cf756fd6244adb8c999425
jillannette <jillarnold2913@gmail.com>
2022-03-29 15:18:59 -0400

    Create DogYears.js

---
+++ DogYears.js
@@ -0,0 +1,9 @@
+const myAge = -1;
+laterYears = myAge - 2;
+let earlyYears = 2;
+earlyYears = earlyYears *= 10.5;
+laterYears *= 4;
+myAgeInDogYears = earlyYears + laterYears;
+myName = 'Jill';
+myName = myName.toLowerCase();
+console.log(`'My name is ${myName}.  I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.'`);
